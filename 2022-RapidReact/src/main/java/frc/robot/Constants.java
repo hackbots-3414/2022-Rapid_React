aa28530cf840e3f5913f29efbe823216a318d999
hackbots <3414hackbots@gmail.com>
2022-01-27 20:26:39 -0500

    Pathweaver - Code on Drivetrain & Constants

--- 2022-RapidReact/src/main/java/frc/robot/Constants.java
+++ 2022-RapidReact/src/main/java/frc/robot/Constants.java
@@ -8,7 +8,6 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package frc.robot;
 
 import javax.xml.namespace.QName;
@@ -16,40 +15,62 @@ import javax.xml.namespace.QName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be
+ * declared globally (i.e. public static). Do not put anything functional in
+ * this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
 
-    private static final Logger LOG = LoggerFactory.getLogger("Constants.java");
-    
-             public static final class DriveConstants {
-              public static final int kLeftMotor1Port = 10;
-              public static final int kLeftMotor2Port = 11;
-             public static final int kRightMotor1Port = 13;
-            public static final int kRightMotor2Port = 14; 
- }
+    public static final Logger LOG = LoggerFactory.getLogger("Constants.java");
 
-            public static final class Transport {
-            
-                public static final int transportMotor1 = 20;
-              public static final int transportMotor2 = 21;
-              public static final int transportMotor3 = 22;
+    public static final double ksVolts = 1.3153;
+    public static final double kvVoltSecondsPerMeter = 5.933;
+    public static final double kaVoltSecondsSquaredPerMeter = 0.75429;
 
+    public static final double kPDriveVel = 8.528;
 
-            }
-            public static final class Intake {
-                public static final int intakeMotor1= 30;
-              
-            }
-            public static final class Shooter { 
-                public static final int shooterMotor1 = 40;
-                public static final int shooterMotor2 = 41;
-            }
+    public static final double kTrackWidthMeters = 0.4925;
+    public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackWidthMeters);
+
+    public static final double kMaxSpeedMetersPerSecond = 1;
+    public static final double kMaxAccelerationMetersPerSecondSquared = 2.5;
+
+    public static final double kRamseteB = 2;
+    public static final double kRamseteZeta = 0.7;
+
+    public static final class DriveConstants {
+        public static final int kLeftMotor1Port = 10;
+        public static final int kLeftMotor2Port = 11;
+        public static final int kRightMotor1Port = 13;
+        public static final int kRightMotor2Port = 14;
+        // MUST BE VALIDATED
+        public static final double testChassisDistancePerTick = 0.00009765625 * Math.PI;
+    }
+
+    public static final class Transport {
+        public static final int transportMotor1 = 20;
+        public static final int transportMotor2 = 21;
+        public static final int transportMotor3 = 22;
+    }
+
+    public static final class Intake {
+        public static final int intakeMotor1 = 30;
+    }
+
+    public static final class Shooter {
+        public static final int shooterMotor1 = 40;
+        public static final int shooterMotor2 = 41;
+    }
 }
-
