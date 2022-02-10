0387564567e9736f2edc3c1510bc851f1566b4c6
Adriaan Friesen <adriaan.friesen@gmail.com>
2022-02-09 19:11:43 -0500

    Added PID configuration from Phoenix tuner to code. Yet to tested.

--- 2022-RapidReact/src/main/java/frc/robot/Constants.java
+++ 2022-RapidReact/src/main/java/frc/robot/Constants.java
@@ -8,7 +8,6 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package frc.robot;
 
 import javax.xml.namespace.QName;
@@ -18,37 +17,41 @@ import org.slf4j.LoggerFactory;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * constants. This class should not be used for any other purpose. All constants should be
+ * declared globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)wherever the
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
 
-    
-             public static final class DriveConstants {
-              public static final int kLeftMotorFrontPort = 10;
-              public static final int kLeftMotorRearPort = 11;
-             public static final int kRightMotorFrontPort = 13;
-            public static final int kRightMotorRearPort = 14; 
- }
+    public static final class DriveConstants {
+        public static final int kLeftMotorFrontPort = 10;
+        public static final int kLeftMotorRearPort = 11;
+        public static final int kRightMotorFrontPort = 13;
+        public static final int kRightMotorRearPort = 14;
+    }
 
-            public static final class Transport {
-            
-                public static final int transportMotor1 = 20;
-              public static final int transportMotor2 = 21;
-              public static final int transportMotor3 = 22;
+    public static final class Transport {
+        public static final int transportMotor1 = 20;
+        public static final int transportMotor2 = 21;
+        public static final int transportMotor3 = 22;
+    }
 
+    public static final class Intake {
+        public static final int intakeMotor1 = 30;
 
-            }
-            public static final class Intake {
-                public static final int intakeMotor1= 30;
-              
-            }
-            public static final class Shooter { 
-                public static final int shooterMotor1 = 40;
-                public static final int shooterMotor2 = 41;
-            }
+    }
+
+    public static final class Shooter {
+        public static final int shooterMotor1 = 40;
+        public static final int shooterMotor2 = 41;
+
+        public static final double integralZone = 201;
+        public static final double kD = 8;
+        public static final double kF = 0.045;
+        public static final double kI = 0.0005;
+        public static final double kP = 0.15;
+    }
 }
-
