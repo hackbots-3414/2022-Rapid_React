a3d4462af8d073e1b6010df899d77cbb536df0a3
Tyler <tylernewton08@gmail.com>
2022-02-10 19:25:11 -0500

    Intake fixed

--- 2022-RapidReact/src/main/java/frc/robot/commands/RunIntake.java
+++ 2022-RapidReact/src/main/java/frc/robot/commands/RunIntake.java
@@ -10,69 +10,40 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.subsystems.Intake;
+import frc.robot.Constants;
 
 public class RunIntake extends CommandBase {
-  /** Creates a new RunIntake. */
+  Intake intake;
+  
+  public RunIntake(Intake m_intake) {
 
-  private final Intake m_intake;
-
-  final WPI_TalonFX intakeMotor1;
-  final WPI_TalonFX intakeMotor2;
-  final WPI_TalonFX intakeMotor3;
-  private static int intakeId1 = Constants.Transport.transportMotor1;
-  private static int intakeId2 = Constants.Transport.transportMotor2;
-  private static int intakeId3 = Constants.Transport.transportMotor3;
-
-  public RunIntake(Intake subsystem) {
-    // Use addRequirements() here to declare subsystem dependencies.
-
-    m_intake = subsystem;
-
+    this.intake = m_intake;
     addRequirements(m_intake);
-
-    intakeMotor1 = new WPI_TalonFX(intakeId1);
-    intakeMotor2 = new WPI_TalonFX(intakeId2);
-    intakeMotor3 = new WPI_TalonFX(intakeId3);
-
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-
-    // Configurable speeds
-    intakeMotor1.set(ControlMode.Velocity, 1000);
-    intakeMotor2.set(ControlMode.Velocity, 1000);
-    intakeMotor3.set(ControlMode.Velocity, 1000);
-    
+    intake.goDown();
+    intake.setSpeed(Constants.Intake.intakeMotorSpeed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    intake.stop();
+    intake.goUp();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-
     return false;
-
-    // This code was commented out because default commands never stop.
-
-    // if ((intakeMotor1.getActiveTrajectoryVelocity() == 0) && (intakeMotor2.getActiveTrajectoryVelocity() == 0)
-    //     && (intakeMotor3.getActiveTrajectoryVelocity() == 0)) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-
   }
 }
