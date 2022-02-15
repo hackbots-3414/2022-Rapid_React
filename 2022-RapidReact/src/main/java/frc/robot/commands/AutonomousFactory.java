05bc77035cb022a0ef66c8484775f5620dbed673
hackbots <3414hackbots@gmail.com>
2022-02-14 20:25:21 -0500

    Pathweaver tests - failed

--- 2022-RapidReact/src/main/java/frc/robot/commands/AutonomousFactory.java
+++ 2022-RapidReact/src/main/java/frc/robot/commands/AutonomousFactory.java
@@ -3,6 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
 import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 
 import org.slf4j.Logger;
@@ -11,6 +14,9 @@ import org.slf4j.LoggerFactory;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
@@ -19,6 +25,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.RobotContainer;
 import frc.robot.TrajectoryFactory;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
@@ -31,22 +39,18 @@ public class AutonomousFactory {
     private static final AutonomousFactory me = new AutonomousFactory();
     private static final Logger LOG = LoggerFactory.getLogger(AutonomousFactory.class);
     public final Drivetrain m_drivetrain = new Drivetrain();
-    private static final LEDFeedback m_ledfeedback = new LEDFeedback();
+    private static final LEDFeedback m_ledfeedback = RobotContainer.getInstance().m_lEDFeedback;
 
     private AutonomousFactory() {
 
     }
 
-    private RamseteCommand createRamseteCommand(Trajectory trajectory) {
+    private RamseteCommand createRamseteCommand(String name, Trajectory trajectory) {
         m_drivetrain.resetHeading();
-
-        var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter), DriveConstants.kDriveKinematics, DriveConstants.maxDriveVoltage);
-
-        TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics).addConstraint(autoVoltageConstraint);
 
         m_drivetrain.resetOdometry(trajectory.getInitialPose());
 
-        RamseteCommand ramseteCommand = new RamseteCommand(trajectory, m_drivetrain::getPose, new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta), new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter), DriveConstants.kDriveKinematics, m_drivetrain::getWheelSpeeds, new PIDController(DriveConstants.kPDriveVel, DriveConstants.kIDriveVel, DriveConstants.kDDriveVel), new PIDController(DriveConstants.kPDriveVel, DriveConstants.kIDriveVel, DriveConstants.kDDriveVel), m_drivetrain::tankDriveVolts, m_drivetrain);
+        RamseteCommand ramseteCommand = new RamseteCommandProxy(name, trajectory, m_drivetrain::getPose, new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta), new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter), DriveConstants.kDriveKinematics, m_drivetrain::getWheelSpeeds, new PIDController(DriveConstants.kPDriveVel, DriveConstants.kIDriveVel, DriveConstants.kDDriveVel), new PIDController(DriveConstants.kPDriveVel, DriveConstants.kIDriveVel, DriveConstants.kDDriveVel), m_drivetrain::tankDriveVolts, m_drivetrain);
 
         m_drivetrain.resetOdometry(trajectory.getInitialPose());
 
@@ -61,9 +65,10 @@ public class AutonomousFactory {
     public Command createShootBackupIntake() {
         SequentialCommandGroup scGroup = new SequentialCommandGroup();
         scGroup.addCommands(createLEDCommand(new Color(255, 0, 0))); // red // scGroup.addCommands(shoot);
-        scGroup.addCommands(new ParallelCommandGroup(createRamseteCommand(TrajectoryFactory.getBlueBottom2Rev()), createLEDCommand(new Color(0, 255, 0))));
+        // scGroup.addCommands(new ParallelCommandGroup(createRamseteCommand(TrajectoryFactory.getBlueBottom2Rev()), createLEDCommand(new Color(0, 255, 0))));
+        scGroup.addCommands(createRamseteCommand("BlueBottom2Rev", TrajectoryFactory.getBlueBottom2Rev()));
         // scGroup.addCommands(intake); // ADD TO PARALLEL 
-        scGroup.addCommands(createRamseteCommand(TrajectoryFactory.getBlueBottom2For()));
+        scGroup.addCommands(createRamseteCommand("BlueBottom2For", TrajectoryFactory.getBlueBottom2For()));
         scGroup.addCommands(createLEDCommand(new Color (0, 0, 255))); // scGroup.addCommands(shoot);
         m_drivetrain.tankDriveVolts(0, 0);
 
@@ -72,5 +77,31 @@ public class AutonomousFactory {
 
     public static AutonomousFactory getInstance() {
         return me;
+    }
+
+    public class RamseteCommandProxy extends RamseteCommand {
+        private String myName;
+        
+        public RamseteCommandProxy(
+            String myName,
+        Trajectory trajectory,
+        Supplier<Pose2d> pose,
+        RamseteController controller,
+        SimpleMotorFeedforward feedforward,
+        DifferentialDriveKinematics kinematics,
+        Supplier<DifferentialDriveWheelSpeeds> wheelSpeeds,
+        PIDController leftController,
+        PIDController rightController,
+        BiConsumer<Double, Double> outputVolts,
+        Subsystem... requirements) {
+            super(trajectory, pose, controller, feedforward, kinematics, wheelSpeeds, leftController, rightController, outputVolts, requirements);
+            this.myName = myName;
+        }
+
+        @Override
+        public void execute() {
+            LOG.trace("{}.execute()", myName);
+            super.execute();
+        }
     }
 }
