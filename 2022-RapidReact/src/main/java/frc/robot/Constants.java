36a9afd908dc3b0152de78c5772e3b113287c7bd
Adriaan Friesen <adriaan.friesen@gmail.com>
2022-03-14 19:33:54 -0400

    Reverted shooter kP value

--- 2022-RapidReact/src/main/java/frc/robot/Constants.java
+++ 2022-RapidReact/src/main/java/frc/robot/Constants.java
@@ -52,7 +52,7 @@ public class Constants {
         public static final double kD = 8;
         public static final double kF = 0.045;
         public static final double kI = 0.0005;
-        public static final double kP = 0.21;
+        public static final double kP = 0.15;
         public static final int shooterTimer = 100000;
     }
 
