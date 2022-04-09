3677a4086064e2f698491888df49ae92ca17b79b
VSingh1012 <72770416+VSingh1012@users.noreply.github.com>
2022-04-09 11:12:12 -0400

    magnetic limit switch

--- 2022-RapidReact/src/main/java/frc/robot/Constants.java
+++ 2022-RapidReact/src/main/java/frc/robot/Constants.java
@@ -82,9 +82,11 @@ public class Constants {
         public static final int climbDownAngle = 180;
         public static final int climberSolenoidChannel_1 = 9;
         public static final int climberSolenoidChannel_2 = 10;
+        public static final int climbMagneticLimitPort = 3; 
     }
 
     public static final class PressureConstants {
         public static final double pressureValue = 80;
     }
+
 }
--- 2022-RapidReact/src/main/java/frc/robot/commands/ClimberDownCommand.java
+++ 2022-RapidReact/src/main/java/frc/robot/commands/ClimberDownCommand.java
@@ -1,9 +1,13 @@
 package frc.robot.commands;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ClimberConstants;
 import frc.robot.subsystems.Climber;
 
 public class ClimberDownCommand extends CommandBase {
     private final Climber m_climber;
+    DigitalInput limitSwitch = new DigitalInput(ClimberConstants.climbMagneticLimitPort);
+
 
     public ClimberDownCommand(Climber subsystem) {
         m_climber = subsystem;
@@ -16,7 +20,16 @@ public class ClimberDownCommand extends CommandBase {
     @Override
     public void execute() {
         m_climber.climberDown();
-    }
+
+            if (limitSwitch.get()) {
+                
+                // We are going up and top limit is tripped so stop
+            } else {
+                // We are going up but top limit is not tripped so go at commanded speed
+                m_climber.climberUp();
+            }
+           }
+    
 
     @Override
     public void end(boolean interrupted) {
