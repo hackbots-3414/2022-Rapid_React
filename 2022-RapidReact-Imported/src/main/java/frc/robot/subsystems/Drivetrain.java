cbd3f652aeb2f1485f54554376501ccbcd63d163
vijay_n7 <vijaynannapuraju@gmail.com>
2024-01-13 14:52:51 -0500

    Updates for 2024, Phoenix 6

--- 2022-RapidReact-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ 2022-RapidReact-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.OpenLoopRampsConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.kauailabs.navx.frc.AHRS;
-import com.kauailabs.navx.frc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,10 +49,10 @@ public class Drivetrain extends SubsystemBase {
         }
 
         public void setOffsets(TalonFX frontLeft, TalonFX backLeft, TalonFX frontRight, TalonFX backRight) {
-            this.frontLeft = frontLeft.getSelectedSensorPosition();
-            this.backLeft = backLeft.getSelectedSensorPosition();
-            this.frontRight = frontRight.getSelectedSensorPosition();
-            this.backRight = backRight.getSelectedSensorPosition();
+            this.frontLeft = frontLeft.getRotorPosition().getValue();
+            this.backLeft = backLeft.getRotorPosition().getValue();
+            this.frontRight = frontRight.getRotorPosition().getValue();
+            this.backRight = backRight.getRotorPosition().getValue();
         }
     };
 
@@ -62,18 +66,32 @@ public class Drivetrain extends SubsystemBase {
     private TalonFX frontRight;
     private DifferentialDrive differentialDrive;
     private DifferentialDriveOdometry m_odometry;
-
-    private CurrentLimitsConfigs frontSupplyLimit = new CurrentLimitsConfigs(false, DriveConstants.driveLowCurrentLimit, DriveConstants.driveLowCurrentLimit, DriveConstants.triggerThresholdTime);
+    private CurrentLimitsConfigs frontSupplyLimit;
+    private CurrentLimitsConfigs highSupplyLimit;
 
     public Drivetrain() {
-        frontLeft = createTalonFX(DriveConstants.kLeftMotorFrontPort, true);
-        backLeft = createTalonFX(DriveConstants.kLeftMotorRearPort, true);
-        frontRight = createTalonFX(DriveConstants.kRightMotorFrontPort, false);
-        backRight = createTalonFX(DriveConstants.kRightMotorRearPort, false);
+        frontLeft = createTalonFX(DriveConstants.kLeftMotorFrontPort, InvertedValue.CounterClockwise_Positive);
+        backLeft = createTalonFX(DriveConstants.kLeftMotorRearPort, InvertedValue.CounterClockwise_Positive);
+        frontRight = createTalonFX(DriveConstants.kRightMotorFrontPort, InvertedValue.Clockwise_Positive);
+        backRight = createTalonFX(DriveConstants.kRightMotorRearPort, InvertedValue.Clockwise_Positive);
+    
+        frontSupplyLimit = new CurrentLimitsConfigs();
+        frontSupplyLimit.SupplyCurrentLimitEnable = false;
+        frontSupplyLimit.SupplyCurrentLimit = DriveConstants.driveLowCurrentLimit;
+        frontSupplyLimit.SupplyCurrentThreshold = DriveConstants.driveLowCurrentLimit;
+        frontSupplyLimit.SupplyTimeThreshold =  DriveConstants.triggerThresholdTime;
 
-        backRight.follow(frontRight);
-        backLeft.follow(frontLeft);
+        highSupplyLimit = new CurrentLimitsConfigs();
+        highSupplyLimit.StatorCurrentLimitEnable = true;
+        highSupplyLimit.StatorCurrentLimit = DriveConstants.driveLowCurrentLimit;
+        highSupplyLimit.SupplyCurrentThreshold = DriveConstants.driveLowCurrentLimit;
+        highSupplyLimit.SupplyTimeThreshold =  DriveConstants.triggerThresholdTime;
 
+
+        backRight.setControl(new Follower(frontRight.getDeviceID(), false));
+        backLeft.setControl(new Follower(frontLeft.getDeviceID(), false));
+        // backRight.follow(frontRight);
+        // backLeft.follow(frontLeft);
         setHighCurrentLimit();
         setLowCurrentLimit();
 
@@ -86,39 +104,66 @@ public class Drivetrain extends SubsystemBase {
         m_odometry = new DifferentialDriveOdometry(ahrs.getRotation2d());
     }
     
-    private TalonFX createTalonFX(int deviceID, boolean isInverted) {
+    private TalonFX createTalonFX(int deviceID, InvertedValue m_invert) {
         TalonFX motor = new TalonFX(deviceID);
-        motor.configFactoryDefault();
-        motor.configOpenloopRamp(DriveConstants.voltageRampRate);
-        motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
-        motor.configSelectedFeedbackSensor(FeedbackConfigs.IntegratedSensor, 0, 10);
-        motor.setSelectedSensorPosition(0, 0, 10);
-        motor.setInverted(isInverted);
+        OpenLoopRampsConfigs openLoopConf = new OpenLoopRampsConfigs();
+        CurrentLimitsConfigs currentLimitConf = new CurrentLimitsConfigs();
+        FeedbackConfigs feedconf = new FeedbackConfigs();
+        MotorOutputConfigs motoroutconf = new MotorOutputConfigs();
+        
+    
+        // motor.configFactoryDefault();
+        motor.getConfigurator().apply(new TalonFXConfiguration());
 
+        openLoopConf.VoltageOpenLoopRampPeriod = DriveConstants.voltageRampRate;
+        motor.getConfigurator().apply(openLoopConf);
+        //motor.configOpenloopRamp(DriveConstants.voltageRampRate);
 
+        currentLimitConf.SupplyCurrentLimitEnable = true;
+        currentLimitConf.SupplyCurrentLimit = DriveConstants.driveCurrentLimit;
+        currentLimitConf.SupplyCurrentThreshold = DriveConstants.driveCurrentLimit;
+        currentLimitConf.SupplyTimeThreshold =  DriveConstants.triggerThresholdTime;
+        motor.getConfigurator().apply(currentLimitConf);
+        //motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
+        
+        feedconf.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+        feedconf.FeedbackRotorOffset = 0; 
+        motor.getConfigurator().apply(feedconf);
+        //motor.configSelectedFeedbackSensor(FeedbackSensorSourceValue.RotorSensor, 0, 10);
+        //motor.setSelectedSensorPosition(0, 0, 10);
+
+        motoroutconf.Inverted = m_invert;
+        motor.getConfigurator().apply(motoroutconf);
+        // motor.setInverted(isInverted);
 
         return motor;
     }
 
     public void setLowCurrentLimit(){
-
-        frontLeft.configSupplyCurrentLimit(frontSupplyLimit);
-        frontRight.configSupplyCurrentLimit(frontSupplyLimit);
+         frontLeft.getConfigurator().apply(frontSupplyLimit);
+         frontRight.getConfigurator().apply(frontSupplyLimit);
+        //frontLeft.configSupplyCurrentLimit(frontSupplyLimit);
+        //frontRight.configSupplyCurrentLimit(frontSupplyLimit);
        }
 
     public void setHighCurrentLimit() {
-        frontRight.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
-        backRight.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
-        backLeft.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
-        frontLeft.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
-    }
+        // frontRight.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
+        // backRight.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
+        // backLeft.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
+        // frontLeft.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, DriveConstants.driveCurrentLimit, DriveConstants.driveCurrentLimit, DriveConstants.triggerThresholdTime));
+        frontRight.getConfigurator().apply(highSupplyLimit);
+        backRight.getConfigurator().apply(highSupplyLimit);
+        backLeft.getConfigurator().apply(highSupplyLimit);
+        frontLeft.getConfigurator().apply(highSupplyLimit);
 
+    }
+    
     public boolean isLowLimitEnabled() {
-        return frontSupplyLimit.enable;
+        return frontSupplyLimit.SupplyCurrentLimitEnable;
     }
 
     public void LowLimitEnable(boolean enable) {
-        frontSupplyLimit.enable = enable;
+        frontSupplyLimit.SupplyCurrentLimitEnable = enable;
         setLowCurrentLimit();
     }
 
@@ -168,7 +213,8 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public double getLeftEncoderPosition() {
-        return ((frontLeft.getSelectedSensorPosition() + backLeft.getSelectedSensorPosition()) / 2D) - encoderOffsets.frontLeft;
+        return ((frontLeft.getRotorPosition().getValue() + backLeft.getRotorPosition().getValue()) / 2D) - encoderOffsets.frontLeft;
+        //return ((frontLeft.getSelectedSensorPosition() + backLeft.getSelectedSensorPosition()) / 2D) - encoderOffsets.frontLeft;
     }
 
     public double getLeftEncoderDistance() {
@@ -176,7 +222,8 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public double getLeftEncoderVelocity() {
-        return frontLeft.getSelectedSensorVelocity();
+        // return frontLeft.getSelectedSensorVelocity();
+        return frontLeft.getRotorVelocity().getValue();
     }
 
     public double getLeftMetersPerSecond() {
@@ -185,7 +232,7 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public double getRightEncoderPosition() {
-        return ((frontRight.getSelectedSensorPosition() + backRight.getSelectedSensorPosition()) / 2D) - encoderOffsets.frontRight;
+        return ((frontRight.getRotorPosition().getValue() + backRight.getRotorPosition().getValue()) / 2D) - encoderOffsets.frontRight;
     }
 
     public double getRightEncoderDistance() {
@@ -193,7 +240,8 @@ public class Drivetrain extends SubsystemBase {
     }
 
     public double getRightEncoderVelocity() {
-        return frontRight.getSelectedSensorVelocity();
+        // return frontRight.getSelectedSensorVelocity();
+        return frontRight.getRotorVelocity().getValue();
     }
 
     public double getRightMetersPerSecond() {
